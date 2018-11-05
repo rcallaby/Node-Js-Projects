f8e147f3eb18441866e64ce67c36a70bcecebbfd
rcallaby <rcallaby@gmail.com>
2018-11-05 10:02:15 -0600

    Just working through the course

---
+++ Node-Course/play.js
@@ -0,0 +1,14 @@
+var name = 'Richard';
+var age = 38
+var hashobbies = true;
+
+function SummarizeUser(userName, userAge, userHasHobbies){
+    return(
+        'The User name is ' + userName + 'the user age is' + userAge + 'and this person has hobbies' + userHasHobbies
+    );
+
+}
+
+console.log(SummarizeUser(name,age,hashobbies));
+
+console.log(name);
